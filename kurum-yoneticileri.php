acecc98f9d2bfcc2a1676cec5cce7842e9f14d4c
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 15:46:59 +0300

    Add separated kurum-yoneticileri.php v1.0.41

---
+++ kurum-yoneticileri.php
@@ -0,0 +1,6 @@
+<?php
+$kyRole='yonetici';
+$kyTitle='Yöneticiler';
+$kyIcon='🧑‍💼';
+$kyDescription='Bu kurumun yönetici hesaplarını ayrı olarak oluştur ve görüntüle.';
+require __DIR__.'/src/kurum_rol_sayfasi.php';
