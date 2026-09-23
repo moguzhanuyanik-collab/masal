aad0262fffdec7aac66bb0eafe1a102e7284e695
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 15:47:09 +0300

    Add separated kurum-ogrencileri.php v1.0.41

---
+++ kurum-ogrencileri.php
@@ -0,0 +1,6 @@
+<?php
+$kyRole='ogrenci';
+$kyTitle='Öğrenciler';
+$kyIcon='🎒';
+$kyDescription='Bu kurumun öğrenci hesaplarını ayrı olarak oluştur ve görüntüle.';
+require __DIR__.'/src/kurum_rol_sayfasi.php';
