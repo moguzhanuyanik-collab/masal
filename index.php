b3fd08c88843ed81215cfeb737381cbfc2908cc5
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 04:16:53 +0300

    Load lesson UI alignment fix v1.0.19

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ require_student_login();
   <link rel="stylesheet" href="styles.css">
   <script src="api/bootstrap.js.php" defer></script>
   <script src="app-runtime.js" defer></script>
+  <script src="lesson-ui-fix.js?v=1.0.19" defer></script>
   <script src="settings-account.js" defer></script>
   <script src="profile-update-link.js" defer></script>
 </head>
