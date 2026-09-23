19746b10a35acb5b0ab80be490320f1077e10900
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 23:35:29 +0300

    Expand super admin SVG icon set

--- src/super_admin_icons.php
+++ src/super_admin_icons.php
@@ -3,10 +3,19 @@
   <symbol id="sa-building" viewBox="0 0 24 24"><path d="M4 21V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v16M8 7h4M8 11h4M8 15h4M16 9h2a2 2 0 0 1 2 2v10M3 21h18"/></symbol>
   <symbol id="sa-users" viewBox="0 0 24 24"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75"/></symbol>
   <symbol id="sa-student" viewBox="0 0 24 24"><path d="m2 9 10-5 10 5-10 5zM6 11.5V16c0 1.8 2.7 3.5 6 3.5s6-1.7 6-3.5v-4.5M22 9v6"/></symbol>
+  <symbol id="sa-teacher" viewBox="0 0 24 24"><path d="M4 20v-2a5 5 0 0 1 5-5h2M9 9a4 4 0 1 0 0-8 4 4 0 0 0 0 8M14 5h7v10h-7zM16.5 9h2M17.5 15v4M15.5 19h4"/></symbol>
   <symbol id="sa-shield" viewBox="0 0 24 24"><path d="M12 3 5 6v5c0 4.7 2.8 8.7 7 10 4.2-1.3 7-5.3 7-10V6zM9 12l2 2 4-5"/></symbol>
   <symbol id="sa-refresh" viewBox="0 0 24 24"><path d="M20 7v5h-5M4 17v-5h5M6.1 8A7 7 0 0 1 18 6l2 1M18 16a7 7 0 0 1-11.9 2L4 17"/></symbol>
   <symbol id="sa-user" viewBox="0 0 24 24"><circle cx="12" cy="8" r="4"/><path d="M4 21v-2a8 8 0 0 1 16 0v2"/></symbol>
   <symbol id="sa-bell" viewBox="0 0 24 24"><path d="M5 17h14l-2-3V9a5 5 0 0 0-10 0v5zM10 21h4"/></symbol>
   <symbol id="sa-lock" viewBox="0 0 24 24"><rect x="5" y="10" width="14" height="11" rx="2"/><path d="M8 10V7a4 4 0 0 1 8 0v3M12 14v3"/></symbol>
+  <symbol id="sa-link" viewBox="0 0 24 24"><path d="M10 13a5 5 0 0 0 7.1.1l2-2A5 5 0 0 0 12 4l-1.1 1.1M14 11a5 5 0 0 0-7.1-.1l-2 2A5 5 0 0 0 12 20l1.1-1.1"/></symbol>
+  <symbol id="sa-check" viewBox="0 0 24 24"><path d="m5 12 4 4L19 6"/></symbol>
+  <symbol id="sa-alert" viewBox="0 0 24 24"><path d="M12 3 2.5 20h19zM12 9v5M12 17h.01"/></symbol>
+  <symbol id="sa-info" viewBox="0 0 24 24"><circle cx="12" cy="12" r="9"/><path d="M12 11v6M12 7h.01"/></symbol>
+  <symbol id="sa-device" viewBox="0 0 24 24"><rect x="7" y="2" width="10" height="20" rx="2"/><path d="M10 18h4"/></symbol>
+  <symbol id="sa-cloud" viewBox="0 0 24 24"><path d="M7 18h10a4 4 0 0 0 .7-7.94A6 6 0 0 0 6.3 8.4 4.5 4.5 0 0 0 7 18Z"/></symbol>
+  <symbol id="sa-code" viewBox="0 0 24 24"><path d="m8 9-4 3 4 3M16 9l4 3-4 3M14 5l-4 14"/></symbol>
+  <symbol id="sa-circle" viewBox="0 0 24 24"><circle cx="12" cy="12" r="5"/></symbol>
   <symbol id="sa-arrow" viewBox="0 0 24 24"><path d="m9 5 7 7-7 7"/></symbol>
 </svg>
