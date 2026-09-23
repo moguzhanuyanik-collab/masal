826d2ca9d8d6b532daca421b8ccc6f21ee5928cf
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 09:47:26 +0300

    Load de-duplicated game feedback audio v1.0.33

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ require_student_login();
   <script src="api/bootstrap.js.php" defer></script>
   <script src="activities-extra.js?v=1.0.31" defer></script>
   <script src="app-runtime.js" defer></script>
-  <script src="global-audio-feedback.js?v=1.0.31" defer></script>
+  <script src="global-audio-feedback.js?v=1.0.33" defer></script>
   <script src="lesson-ui-fix.js?v=1.0.32" defer></script>
   <script src="settings-account.js" defer></script>
   <script src="profile-update-link.js" defer></script>
