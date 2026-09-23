3bccd58912d0a24386f7750380873f29f35c882b
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 03:10:34 +0300

    Add v1.0.11 auth repair marker

---
+++ database/migrations/005_auth_repair.sql
@@ -0,0 +1,5 @@
+-- V1.0.11
+-- Ogrenci auth tablolari src/updater.php tarafinda her kurulumda kontrol edilir.
+-- Tablo yoksa olusturulur, varsa eksik kolonlar tamamlanir.
+-- Test hesap: masal@gmail.com
+SET NAMES utf8mb4;
