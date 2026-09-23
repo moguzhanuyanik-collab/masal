7aee81314d2c9af114866ecbaa01e9ab31c2e201
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 23:09:19 +0300

    Add shared super admin SVG icon library

---
+++ src/super_admin_icons.php
@@ -0,0 +1,12 @@
+<svg class="sa-sub-icon-library" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <symbol id="sa-home" viewBox="0 0 24 24"><path d="M3 10.8 12 3l9 7.8v9.2a1 1 0 0 1-1 1h-5v-6H9v6H4a1 1 0 0 1-1-1z"/></symbol>
+  <symbol id="sa-building" viewBox="0 0 24 24"><path d="M4 21V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v16M8 7h4M8 11h4M8 15h4M16 9h2a2 2 0 0 1 2 2v10M3 21h18"/></symbol>
+  <symbol id="sa-users" viewBox="0 0 24 24"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75"/></symbol>
+  <symbol id="sa-student" viewBox="0 0 24 24"><path d="m2 9 10-5 10 5-10 5zM6 11.5V16c0 1.8 2.7 3.5 6 3.5s6-1.7 6-3.5v-4.5M22 9v6"/></symbol>
+  <symbol id="sa-shield" viewBox="0 0 24 24"><path d="M12 3 5 6v5c0 4.7 2.8 8.7 7 10 4.2-1.3 7-5.3 7-10V6zM9 12l2 2 4-5"/></symbol>
+  <symbol id="sa-refresh" viewBox="0 0 24 24"><path d="M20 7v5h-5M4 17v-5h5M6.1 8A7 7 0 0 1 18 6l2 1M18 16a7 7 0 0 1-11.9 2L4 17"/></symbol>
+  <symbol id="sa-user" viewBox="0 0 24 24"><circle cx="12" cy="8" r="4"/><path d="M4 21v-2a8 8 0 0 1 16 0v2"/></symbol>
+  <symbol id="sa-bell" viewBox="0 0 24 24"><path d="M5 17h14l-2-3V9a5 5 0 0 0-10 0v5zM10 21h4"/></symbol>
+  <symbol id="sa-lock" viewBox="0 0 24 24"><rect x="5" y="10" width="14" height="11" rx="2"/><path d="M8 10V7a4 4 0 0 1 8 0v3M12 14v3"/></symbol>
+  <symbol id="sa-arrow" viewBox="0 0 24 24"><path d="m9 5 7 7-7 7"/></symbol>
+</svg>
