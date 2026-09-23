47de4dd2dc4d3394d8975e0c74e33d44a33f466d
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 15:47:03 +0300

    Add separated kurum-ogretmenleri.php v1.0.41

---
+++ kurum-ogretmenleri.php
@@ -0,0 +1,6 @@
+<?php
+$kyRole='ogretmen';
+$kyTitle='Öğretmenler';
+$kyIcon='👩‍🏫';
+$kyDescription='Bu kurumun öğretmen hesaplarını ayrı olarak oluştur ve görüntüle.';
+require __DIR__.'/src/kurum_rol_sayfasi.php';
