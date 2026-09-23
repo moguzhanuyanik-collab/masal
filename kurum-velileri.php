f728d4ec3133030cb734c66c211c288eeac089f4
moguzhanuyanik-collab <m.oguzhanuyanik@gmail.com>
2026-09-23 15:47:05 +0300

    Add separated kurum-velileri.php v1.0.41

---
+++ kurum-velileri.php
@@ -0,0 +1,6 @@
+<?php
+$kyRole='veli';
+$kyTitle='Veliler';
+$kyIcon='👪';
+$kyDescription='Bu kurumun veli hesaplarını ayrı olarak oluştur ve görüntüle.';
+require __DIR__.'/src/kurum_rol_sayfasi.php';
